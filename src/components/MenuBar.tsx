07b29defb65415db719cac964012040b29d7b1d3
jconan <conanjahh@hotmail.com>
2020-07-22 20:48:31 +0200

    add some tweak in menuitem style

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import "./MenuBar.sass"
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import "./MenuBar.sass";
+import { Link, useLocation } from "react-router-dom";
 
-export interface MenuBarProps {
-
-}
+export interface MenuBarProps {}
 
 const MenuBar: React.SFC<MenuBarProps> = () => {
-    let location = useLocation()
+  let location = useLocation();
 
-    let buildClass = (path: string) =>
-        location.pathname === path ? "menuitem menuitem-active" : "menuitem"
+  let buildClass = (path: string) =>
+    location.pathname === path ? "menuitem menuitem-active" : "menuitem";
 
-    return (
-        <nav className="menubar">
-            <ul className="menulist">
-                <li><header>My Portofolio</header></li>
-                <li className={buildClass("/vidly")}><Link to="/vidly">Vidly</Link></li>
-                <li className={buildClass("/")}><Link to="/">Home</Link></li>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className="menubar">
+      <ul className="menulist">
+        <li>
+          <div className="banner">My Portofolio</div>
+        </li>
+        <li className={buildClass("/vidly")}>
+          <Link to="/vidly">Vidly</Link>
+        </li>
+        <li className={buildClass("/")}>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default MenuBar;
