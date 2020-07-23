4486725902a27fdc5311376a6aaff258b6939c85
jconan <conanjahh@hotmail.com>
2020-07-24 01:30:55 +0200

    put back component content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import MenuBar from "./components/MenuBar";
 import "./App.sass";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="app">
       <MenuBar />
+      <main>
+        <Content />
+      </main>
     </div>
   );
 }
