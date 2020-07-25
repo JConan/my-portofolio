c729bb17d38b855284ae5813ad60139defafee57
jconan <conanjahh@hotmail.com>
2020-07-25 22:03:30 +0200

    configure basic dev environment
    yarn dev to startup yarn start@react-ui & nodemon server.
    This is done in order to provide a better DevX as the change on the 'server' and 'react-ui' should now reflected in real time

--- react-ui/src/setupProxy.js
+++ react-ui/src/setupProxy.js
@@ -1,5 +1,16 @@
-module.exports = function (app) {
-  app.use("/api", (req, resp, next) => {
-    resp.send("hello");
-  });
-};
+const isDev = process.env.NODE_ENV !== "production";
+if (isDev) {
+  const morgan = require("morgan");
+  const { createProxyMiddleware } = require("http-proxy-middleware");
+  module.exports = function (app) {
+    app.use(morgan("combined"));
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        pathRewrite: (p) => p.replace(/^\/api\/(.*)/, "/api/$1"),
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      })
+    );
+  };
+}
--- server/index.js
+++ server/index.js
@@ -7,17 +7,14 @@ const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
 
 const isDev = process.env.NODE_ENV !== "production";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
   console.error(`Node cluster master ${process.pid} is running`);
-
-  // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
-
   cluster.on("exit", (worker, code, signal) => {
     console.error(
       `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
@@ -25,10 +22,7 @@ if (!isDev && cluster.isMaster) {
   });
 } else {
   const app = express();
-
-  // Priority serve any static files.
   app.use(morgan("combined"));
-  // Proxy Todos API from JsonPlaceHolder
   app.use(
     "/api/todos",
     createProxyMiddleware({
@@ -38,26 +32,16 @@ if (!isDev && cluster.isMaster) {
     })
   );
 
-  app.get("/api/hello", (_, res) => res.send("Hello :)"));
+  if (!isDev) {
+    // security package
+    app.use(require("helmet")());
 
-  //app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
-  app.use(express.static(path.join(__dirname, "../react-ui/build")));
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-
-  // Answer API requests.
-  // app.get("/api", function (req, res) {
-  //   res.set("Content-Type", "application/json");
-  //   res.send('{"message":"Hello from the custom server!"}');
-  // });
-
-  // // All remaining requests return the React app, so it can handle routing.
-  // app.get("*", function (request, response) {
-  //   response.sendFile(
-  //     path.resolve(__dirname, "../react-ui/build", "index.html")
-  //   );
-  // });
+    // PRODUCTION : serve 'react-ui'
+    app.use(express.static(path.join(__dirname, "../react-ui/build")));
+    app.get("/", function (req, res) {
+      res.sendFile(path.join(__dirname, "../react-uibuild", "index.html"));
+    });
+  }
 
   app.listen(PORT, function () {
     console.error(
