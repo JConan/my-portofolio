03afc02ee5453f932c2b55a13d29bcf472dd33f4
jconan <conanjahh@hotmail.com>
2020-08-01 23:21:36 +0200

    add another suite test for db.inMemory
    - example to show how to do insert beforeEach / drop afterEach

--- server/db.inMemory.test.ts
+++ server/db.inMemory.test.ts
@@ -3,10 +3,9 @@ import mongoose, { Schema } from "mongoose"
 
 
 describe('in mememory db - for unit testing purpose', () => {
+    const dbServer = dbInMemory.createServerConnection()
 
     describe('connect & disconnect mutiple times', () => {
-        const dbServer = dbInMemory.createServerConnection()
-
         beforeEach(async () => {
             expect.assertions(5);
             expect(dbServer.getUri()).toBe(false);
@@ -23,10 +22,35 @@ describe('in mememory db - for unit testing purpose', () => {
             expect(mongoose.connection.readyState).not.toBe(mongoose.STATES.connected);
         })
 
-        it.each([1, 2, 3, 4, 5])('should work', () => {
+        it.each([1, 2, 3])('should able to check connection state', () => {
             expect(dbServer.getUri()).toMatch(/^mongodb:\/\/127.0.0.1:[0-9]+.*/i);
             expect(mongoose.connection.readyState).toBe(mongoose.STATES.connected);
         })
+    })
+
+    describe('test insert/remove data from a connection', () => {
+        const model = mongoose.model('test', new Schema({}, { strict: false }))
+        beforeAll(async () => await dbServer.connect())
+        afterAll(async () => await dbServer.disconnect());
+
+        beforeEach(async () => {
+            expect.assertions(3)
+            const result = await model.collection.insertMany([
+                { _id: mongoose.Types.ObjectId("5f258b1eb04cac066446b960"), test: "inMemory" },
+                {},
+                {}])
+            expect(result.insertedCount).toBe(3)
+        })
+        afterEach(async () => await model.collection.drop())
+
+        it.each([1, 2, 3])('should be able to retrieve data', async () => {
+            const allDatas = await model.find().exec()
+            expect(allDatas).toHaveLength(3)
+
+            const oneData = await model.findById("5f258b1eb04cac066446b960").exec()
+            expect(oneData?.toJSON()).toHaveProperty('test', 'inMemory')
+        })
+
     })
 
 })
